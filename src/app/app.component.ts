25c67dd2986f680bbd194cc10a4074369655ed35
FelipeLabadessaC:/Users/Felipe Labadessa/AppData/Local/Programs/Python/Python310/python.exe <felipe_labadessa@outlook.com>
2024-06-09 15:13:06 -0300

    folder change 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EventosComponent } from './pages/eventos/eventos.component';
-import { PalestrantesComponent } from './pages/eventos/palestrantes/palestrantes.component';
+import { PalestrantesComponent } from './pages/palestrantes/palestrantes.component';
 
 @Component({
   selector: 'app-root',
